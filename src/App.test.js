7fb86a3645d5b2d46784bf43e51ee521e9c36890
hughesblues94 <hughesblues94@gmail.com>
2022-11-12 12:49:29 +0000

    added a test that checks the text content of the App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './Components/App';
 
-test('that the component renders the brewdog name', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Brewdog/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('correct text is displayed', () => {
+
+  test('that the component renders the brewdog name', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Brewdog/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('that the button displays the correct text', () => {
+    render(<App />)
+    const beerGetter = screen.getByText(/Get All The Beers/i);
+    expect(beerGetter).toBeInTheDocument();
+  })
+})
+
+
+
